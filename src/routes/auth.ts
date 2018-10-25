3f09373c5b99957d31a21380013fc9c2eb59d727
bbenefield89 <bsquared18@gmail.com>
2018-10-25 17:31:28 -0400

    INDEX POST ROUTE '/' NOW RETURNS AUTHENTICATED USER
    - src/routes/auth.ts

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,16 +1,16 @@
-  /**
-   * TODO: change this route to the signup/login route
-   */
-  export const authGet = ((_req, res) => {
-    const locals = {
-      message: 'Signup/Login',
-      pageTitle: 'Overview'
-    }
-    
-    res.render('index', locals)
-  })
+/**
+ * TODO: change this route to the signup/login route
+ */
+export const authGet = ((_req, res) => {
+  const locals = {
+    message: 'Signup/Login',
+    pageTitle: 'Overview'
+  }
+  
+  res.render('index', locals)
+})
 
-  export const authPost = ((req, res) => {
-    console.log(req.body)
-    res.send({ data: req.body })
-  })
+export const authPost = ((_req, res) => {
+  const { admin } = res.locals
+  res.send({ data: admin })
+})
