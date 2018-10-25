3488604d9d174245988cd1f6dd93be6157271afc
bbenefield89 <bsquared18@gmail.com>
2018-10-25 15:59:23 -0400

    NEW DB MODEL 'admin'
    - src/models/Admin.ts

---
+++ src/models/Admin.ts
@@ -0,0 +1,27 @@
+import Sequelize from 'sequelize'
+
+export const Admin = db => {
+  const adminModel = db.define(
+    'admins',
+    {
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          isAlphanumeric: true,
+          len: [1, 256]
+        }
+      },
+
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          len: [1, 256]
+        }
+      }
+    }
+  )
+
+  return adminModel
+}
