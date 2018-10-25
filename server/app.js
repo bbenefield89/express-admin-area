fde98d918ee92f9695d4a7d584641e8a293c887d
bbenefield89 <bsquared18@gmail.com>
2018-10-24 22:20:10 -0400

    REMOVE 'server/' DIRECTORY
    - server/

--- server/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const { adminArea } = require('../build/src/main')
-
-const app = express()
-
-app.use('/admin', adminArea)
-
-app.listen(3000, () => console.log('\n\nServer Online\n\n'))
