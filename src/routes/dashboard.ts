87724cf3dc8946ec063dd9844e5ebf068dd8d782
bbenefield89 <bsquared18@gmail.com>
2018-10-27 16:55:49 -0400

    PLACEHOLDER POST ROUTE '/dashboard'
    - src/routes/dashboard.ts
      - this route will handle table CRUD, this is a pretty complex process with tons of options and I will save this for a later date

--- src/routes/dashboard.ts
+++ src/routes/dashboard.ts
@@ -1,4 +1,4 @@
-const dashboardGet = async (_req, res) => {
+const dashboardGet = async (req, res) => {
   const { db } = res.locals
   const tables = await db.query(
     `
@@ -10,11 +10,24 @@ const dashboardGet = async (_req, res) => {
   const tableNames = tables[0].map(table => {
     return {
       tableName: table.tablename,
-      url: _req.originalUrl
+      url: req.originalUrl
     }
   })
 
   res.render('dashboard', { tableNames, pageTitle: 'Dashboard' })
 }
 
-export { dashboardGet }
+/**
+ * @todo this will later be used to create/delete/modify new tables in the db
+ *       for now though this will sit here and be useless
+ */
+const dashboardPost = async (req, res) => {
+  // const { db } = res.locals
+  const { tableName, ...rest } = req.body
+  
+  res.send(rest)
+}
+
+export { dashboardGet, dashboardPost }
+
+// curl -X POST -H "Content-Type: application/json" -d '{"tableName": "users", "username": "{"type": "Sequelize.STRING", "allowNull": false, }"}'
