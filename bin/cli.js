69db552c25d68a08a60a618cec3a4ec08593d0a4
bbenefield89 <bsquared18@gmail.com>
2018-10-28 15:55:18 -0400

    REMOVE UNNECESSARY CODE/FUNCTIONS/IMPORTS
    - bin/cli.js
      - now that all supported DB drivers, e.g. pg, mysql2, etc, come with this package by default there is no need to assess which database the user is using
      - this means a lot of the logic inside this script was no longer needed and can be removed
      - now this script will only handle creating a new user in the 'admins' table

--- bin/cli.js
+++ bin/cli.js
@@ -4,65 +4,10 @@
  * TODO: use a promise library, bluebird or node built in util.promisify to
  *       help refactor this mess
  */
-const fs = require('fs')
-const cp = require('child_process')
 const Sequelize = require('sequelize')
 const prompt = require('prompt')
 
 const { Admin } = require('../build/src/models/Admin')
-
-let dbDialect = ''
-
-/**
- * @summary reads the argument passed into the command prompt and decides
- *          which database dialect to use with Sequelize,
- *          e.g. postgres: pg, sqlite: sqlite3
- * @return {string}
- */
-const setDBDialect = () => {
-  /**
-   * TODO: right now this will only work for PostgreSQL DB's
-   *       need to figure out what 'sqlite', 'mysql', and 'mssql' DB urls look
-   *       like
-   *       and handle these conditions accordingly
-   */
-  const dbConnUrl = process.argv[2]
-
-  if (dbConnUrl.includes('postgresql')) {
-    dbDialect = 'pg pg-hstore'
-  }
-
-  return dbDialect
-}
-
-setDBDialect()
-
-/**
- * @summary looks through the root directory for either a package-lock.json or
- *          a yarn.lock file to determine which package manager to use to
- *          download the database dialect from NPM
- * @return {Promise}
- */
-const setPackageManager = () => new Promise((res, rej) => {
-  let packageManager = ''
-
-  fs.readdir('./', (err, data) => {
-    if (err)
-      throw new Error(err)
-  
-    if (/package-lock.json/.test(data)) {
-      packageManager = 'npm'
-    }
-    else if (/yarn.lock/.test(data)) {
-      packageManager = 'yarn'
-    }
-    else {
-      rej('No package manager found')
-    }
-
-    res(packageManager)
-  })
-})
 
 /**
  * @summary using the prompt library from NPM, this function asks the user for
@@ -75,7 +20,7 @@ const getAdminInfo = async () => {
   
     prompt.get(['username', 'password'], (err, response) => {
       if (err) {
-        throw new Error(err)
+        rej(err)
       }
   
       res(response)
@@ -87,45 +32,20 @@ const getAdminInfo = async () => {
  * @summary creates a new admin in the database
  * @return {void}
  */
-const fetchAdmins = async () => {
-  const db = new Sequelize(process.argv[2])
-  const admin = await Admin(db)
-  const adminInfo = await getAdminInfo()
-  const { username } = await admin.create(adminInfo)
-
-  console.log(`New admin ${ username } successfully created.`)
-  process.exit()
-}
-
-/**
- * @summary installs the correct database dialect
- * @return {void}
- */
-const installDependencies = async () => {
+const createNewAdmin = async () => {
   try {
-    const packageManager = await setPackageManager()
-    const installDepsCommand = packageManager === 'npm' ? 'npm install ' : 'yarn add '
-    
-    console.log(`Installing Dependencies: ${ dbDialect }`)
-
-    cp.exec(installDepsCommand + dbDialect, (err, sout, serr) => {
-      if (err) {
-        throw new Error(err)
-      }
-    
-      if (serr) {
-        console.log('STDERR:', serr)
-      }
-    
-      if (sout) {
-        console.log('STDOUT:', sout)
-        fetchAdmins()
-      }
-    })
+    const db = new Sequelize(process.argv[2])
+    const admin = await Admin(db)
+    const adminInfo = await getAdminInfo()
+    const { username } = await admin.create(adminInfo)
+  
+    console.log(`New admin ${ username } successfully created.`)
   }
   catch(err) {
     throw new Error(err)
   }
+  
+  process.exit()
 }
 
-installDependencies()
+createNewAdmin()
