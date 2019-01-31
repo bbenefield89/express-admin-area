c26d5dbdf4bf3c87fe45638c48af4a3495b8ee2c
bbenefield89 <bsquared18@gmail.com>
2019-01-30 23:36:12 -0500

    SERVICE CLASS HANDLES BUSINESS LOGIC FOR 'AuthenticateAdminController'
    - src/services/AuthenticateAdminService/AuthenticateAdminService.ts

--- src/services/AuthenticateAdminService/AuthenticateAdminService.ts
+++ src/services/AuthenticateAdminService/AuthenticateAdminService.ts
@@ -5,38 +5,35 @@ import Admin from '../../models/Admin'
 
 class AuthenticateAdminService {
 
-  constructor() {}
+  public static async authenticateAdmin(req, res, next): Promise<void> {
+    res.locals.token = { error: 'Username or password combination is incorrect' }
+    const adminEntity = await AuthenticateAdminService.findAdminByUsername(req, res)
+    const passwordsMatch: Boolean = await AuthenticateAdminService.checkIfPasswordsMatch(
+      req.body.password,
+      adminEntity.password
+    )
+    if (passwordsMatch) {
+      res.locals.token = await AuthenticateAdminService.generateJwt(adminEntity)
+    }
+    next()
+  }
 
-  public static authenticateAdmin(req, res, next): void {
-    const { databaseConnection } = res.locals
+  private static async findAdminByUsername(req, res): Promise<any> {
+    const databaseConnection: Object = res.locals.databaseConnection
     const adminModel = Admin(databaseConnection)
-    
-    adminModel.findOne({
-      where: {
-        username: req.body.username
-      }
-    })
-      .then(record => {
-        bcrypt.compare(req.body.password, record.password)
-          .then(passwordsMatch => {
-            if (passwordsMatch) {
-              jwt.sign({
-                  id: record.id,
-                  username: record.username
-                },
-                'expressadminarea',
-                (_err, token) => {
-                  res.locals.token = token
-                  next()
-              })
-            }
-          })
-      })
-      .catch(err => {
-        console.log(err)
-        console.log('\n\nError: AuthenticateAdminService.ts (authenticateAdmin)\n\n')
-        next()
-      })
+    const findOneWhereClause: Object = { where: { username: req.body.username }}
+    const adminEntity: Object = await adminModel.findOne(findOneWhereClause)
+    return adminEntity
+  }
+
+  private static async checkIfPasswordsMatch(plainTextPw: String, hashedPw: String): Promise<Boolean> {
+    return await bcrypt.compare(plainTextPw, hashedPw)
+  }
+
+  private static async generateJwt(adminEntity): Promise<String> {
+    const { id, username }: { id: Number, username: String } = adminEntity
+    const token: String = await jwt.sign({ id, username }, 'expressadminarea')
+    return token
   }
   
 }
