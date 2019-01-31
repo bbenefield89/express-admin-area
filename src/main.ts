e3b774e76710cf728882bf9721aaa967f9a895a0
bbenefield89 <bsquared18@gmail.com>
2019-01-30 23:22:49 -0500

    PULL OFF BROAD METHODS INTO THEIR OWN CLASSES
    - src/main.ts
      - remove unused/commented code

--- src/main.ts
+++ src/main.ts
@@ -1,110 +1,27 @@
 import * as Sequelize from 'sequelize'
 
 import RoutesController from './controllers/RoutesController'
-// import * as routes from './routes'
-// import { Admin } from './models/Admin'
-// import { Router } from 'express';
+import MiddlewareRegisterer from './middlewares/MiddlewareRegisterer/MiddlewareRegisterer'
+import ViewRegisterer from './middlewares/ViewRegisterer/ViewRegisterer'
 
 class ExpressAdminArea {
 
-  private static express
-  private static router
-  private static databaseConnection
-  private static databaseTables
+  private static express;
+  private static router;
+  private static databaseConnection;
+  private static databaseTables;
   
   public static init(express, databaseUri: string, databaseTables: object) {
-    ExpressAdminArea.express = express
-    ExpressAdminArea.router = express.Router()
-    ExpressAdminArea.databaseConnection = new Sequelize(databaseUri)
-    ExpressAdminArea.databaseTables = databaseTables
-    ExpressAdminArea.registerMiddleware()
-    return ExpressAdminArea.router
+    this.express = express
+    this.router = express.Router()
+    this.databaseConnection = new Sequelize(databaseUri)
+    this.databaseTables = databaseTables
+    MiddlewareRegisterer.registerMiddlewares(this)
+    RoutesController.registerAllRoutes(this.router)
+    ViewRegisterer.registerViews(this)
+    return this.router
   }
-
-  private static registerMiddleware(): void {
-    ExpressAdminArea.allowContentTypeApplicationJson()
-    ExpressAdminArea.registerGlobalVariables()
-    RoutesController.registerAllRoutes(ExpressAdminArea.router)
-    ExpressAdminArea.registerViewsLocation()
-  }
-
-  private static allowContentTypeApplicationJson(): void {
-    ExpressAdminArea.router.use(ExpressAdminArea.express.json())
-  }
-
-  private static registerGlobalVariables(): void {
-    ExpressAdminArea.router.use((req, res, next) => {
-      const http = req.protocol
-      const domainName = req.get('host')
-      res.locals.baseUrl = `${ http }://${ domainName }/expressadminarea`
-      res.locals.databaseConnection  = ExpressAdminArea.databaseConnection
-      next()
-    })
-  }
-
-  private static registerViewsLocation() {
-    const pathToViews = __dirname + '/views'
-    const serveStaticFiles = ExpressAdminArea.express.static(pathToViews)
-    ExpressAdminArea.router.use('/expressadminarea', serveStaticFiles)
-    ExpressAdminArea.router.use('/expressadminarea/?*', serveStaticFiles)
-  }
-
-  public static registerAllRoutes(): void {}
 
 }
 
-// const adminAreaConfig = (express, db, models: Object) => {
-//   const adminArea = express.Router()
-//   const adminModel = Admin(db)
-
-//   /**
-//    * Model names need to be lowercase to easier grab the correct model in our
-//    * model-dependent routes. For an example look at the 'tableDataGet' route
-//    */
-//   const lowerCasedModels = { admins: adminModel }
-//   for (let modelName in models) {
-//     lowerCasedModels[ modelName.toLowerCase() ] = models[ modelName ]
-//   }
-
-//   // configure 'express-admin-area' middleware
-//   adminArea.use(express.json())
-//   adminArea.use((_req, res, next) => {
-//     res.locals.db = db
-//     res.locals.models = lowerCasedModels
-//     next()
-//   })
-//   adminArea.use((req, res, next) => {
-//     const http = req.protocol
-//     const domainName = req.get('host')
-//     res.locals.baseUrl = `${ http }://${ domainName }/expressadminarea`
-//     next()
-//   })
-//   adminArea.use('/expressadminarea', express.static(__dirname + '/views'))
-
-//   /**
-//    * ROUTES
-//    */
-//   // baseUrl
-//   RoutesController.registerAllRoutes(adminArea)
-  
-//   // log in/authentication
-//   // routes.authRoutes(adminArea, db)
-
-//   // dashboard - this is where users will see a list of tables from the DB
-//   adminArea.get('/dashboard', routes.dashboardGet)
-//   adminArea.post('/dashboard', routes.dashboardPost)
-
-//   // tableData - view information about individual tables
-//   adminArea.get('/dashboard/:tableName', routes.tableDataGet)
-//   adminArea.post('/dashboard/:tableName', routes.tableDataPost)
-//   adminArea.delete('/dashboard/:tableName', routes.tableDataDelete)
-//   adminArea.put('/dashboard/:tableName', routes.tableDataPut)
-  
-//   // create 'admin' table in database
-//   adminModel.sync()
-
-//   return adminArea
-// }
-
-// export { adminAreaConfig }
 export { ExpressAdminArea }
