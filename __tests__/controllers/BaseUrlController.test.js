29f6c52e5384ad3806f12f63182e1f3359aea912
bbenefield89 <bsquared18@gmail.com>
2019-01-31 20:45:41 -0500

    TESTS PASSING
    - __tests__/controllers/BaseUrlController.test.js

--- __tests__/controllers/BaseUrlController.test.js
+++ __tests__/controllers/BaseUrlController.test.js
@@ -4,21 +4,26 @@ const express = require('express')
 const ExpressAdminArea = require('../../index')
 
 const app = express()
-const expressAdminArea = ExpressAdminArea.init(express)
+const expressAdminArea = ExpressAdminArea.init(express, 'postgresql://postgres:root@127.0.0.1/express_admin_area', {})
 
 app.use(express.json())
 app.use(expressAdminArea)
 
 
 describe('/expressadminarea/baseurl', () => {
-  it('should return "http://localhost:3000/expressadminarea/baseurl', () => {
-    request(app).get('/expressadminarea')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', "application/json")
+  it('should return "http://localhost:3000/expressadminarea/baseurl"', done => {
+    request(app).get('/expressadminarea/baseurl')
+      .expect(res => {
+        res.body.data = 'http://localhost:3000/expressadminarea/baseurl'
+      })
+      .expect('Content-Type', 'application/json; charset=utf-8')
+      .expect(200)
+      .expect({ data: 'http://localhost:3000/expressadminarea/baseurl' })
       .end((err, res) => {
         if (err) {
-          console.log(err)
+          done(err)
         }
+        done()
       })
   })
 })
