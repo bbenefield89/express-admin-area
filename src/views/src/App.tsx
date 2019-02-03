fc1d5b32779c8fcb715cd8efac4fdfd35deb20e3
bbenefield89 <bsquared18@gmail.com>
2019-02-03 16:30:36 -0500

    CREATE ADMIN LOG OUT METHOD
    - src/views/src/App.tsx

--- src/views/src/App.tsx
+++ src/views/src/App.tsx
@@ -1,63 +1,94 @@
 import React, { Component } from 'react';
-import './App.css';
 import { Route } from 'react-router-dom'
 
 import AdminLogin from './components/AdminLogin/AdminLogin'
+import Dashboard from './components/Dashboard/Dashboard'
 
-class App extends Component {
+import './App.css';
+
+type State = { baseUrl: String }
+class App extends Component<Object, State> {
 
   state = {
     baseUrl: ''
   }
   
-  render() {
-    this.handleCheckIfAdminJwtIsSet()
+  /**
+   * render
+   */
+  render(): any  {
     return (
       <React.Fragment>
-        <Route exact path='/' component={ AdminLogin } />
+        <nav>
+          <button onClick={ this.adminLogOut }>Log Out</button>
+        </nav>
+        <Route exact path='/' render={ props => this.renderAdminLoginComponent(props) } />
+        <Route path='/dashboard' component={ Dashboard } />
       </React.Fragment>
     )
   }
 
-  /**
-   * componentDidMount
-   */
-  componentDidMount() {
-    this.handleSetServerBaseUrl()
+  public adminLogOut = (): void => {
+    localStorage.removeItem('token')
+    window.location.href = '/expressadminarea'
   }
   
   /**
-   * handleCheckIfAdminJwtIsSet
+   * componentDidMount
    */
-  handleCheckIfAdminJwtIsSet = (): void => {
-    const adminJwt = localStorage.getItem('token')
-    const currentUrlEqualsServersBaseUrl = (window.location.href === this.state.baseUrl + '/')
+  public async componentWillMount(): Promise<void> {
+    await this.setBaseUrl()
+    this.checkIfAdminJwtIsSet()
+  }
+
+  /**
+   * renderAdminLoginComponent
+   */
+  private renderAdminLoginComponent = (props: Object) => {
+    return (
+      <AdminLogin
+        { ...props }
+        checkIfAdminJwtIsSet={ this.checkIfAdminJwtIsSet }
+      />
+    )
+  }
+
+  /**
+   * checkIfAdminJwtIsSet
+   */
+  public checkIfAdminJwtIsSet = (): void => {
+    const adminJwt: String | null = localStorage.getItem('token')
     if (adminJwt) {
-      if (currentUrlEqualsServersBaseUrl) {
-        window.location.href = `${ this.state.baseUrl }/dashboard`
-      }
+      this.checkIfUrlAndBaseUrlMatch()
     }
   }
 
   /**
-   * handleSetServerBaseUrl
+   * checkIfUrlAndBaseUrlMatch
    */
-  handleSetServerBaseUrl = (): void => {
-    this.handleFetchBaseUrl()
+  private checkIfUrlAndBaseUrlMatch = (): void => {
+    const currentUrlEqualsServersBaseUrl: Boolean = (window.location.href === this.state.baseUrl + '/')
+    if (currentUrlEqualsServersBaseUrl) {
+      this.redirectUserToDashboard()
+    }
   }
 
   /**
-   * handleFetchBaseUrl
+   * redirectUserToDashboard
    */
-  handleFetchBaseUrl = (): void => {
-    fetch('/expressadminarea/baseurl')
-      .then(data => data.json())
-      .then(({ data }) => {
-        this.setState({ baseUrl: data })
-      })
-      .catch(() => console.log('\n\nError: "App.tsx (handleFetchBaseUrl)"'))
+  private redirectUserToDashboard = (): void => {
+    window.location.href = `${ this.state.baseUrl }/dashboard`
   }
-  
+
+  /**
+   * setBaseUrl
+   */
+  private setBaseUrl = async (): Promise<void> => {
+    return fetch('/expressadminarea/api/baseurl')
+      .then(res => res.json())
+      .then(({ data }) => this.setState({ baseUrl: data }))
+  }
+
 }
 
 export default App;
