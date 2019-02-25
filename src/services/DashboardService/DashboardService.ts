b8fbe25ab075a581e686093262883b8c31b4547a
bbenefield89 <bsquared18@gmail.com>
2019-02-24 21:04:38 -0500

    SETS ARRAY OF PUBLIC DATABASE TABLES TO 'res.locals.dbTables'
    - src/services/DashboardService/DashboardService.ts

--- src/services/DashboardService/DashboardService.ts
+++ src/services/DashboardService/DashboardService.ts
@@ -1,10 +1,9 @@
 class DashboardService {
 
-  public static async getTablesQuery(databaseConnection): Promise<Array<string>> {
-    const showTablesQuery: string = databaseConnection.queryInterface.QueryGenerator.showTablesQuery()
-    const databaseTablesFromQuery: Array<Array<string>> = await databaseConnection.query(showTablesQuery)
-    const formattedDatabaseTables: Array<string> = databaseTablesFromQuery.map(table => table[0])
-    return formattedDatabaseTables
+  public static async getTables(req, res, next) {
+    const { models } = res.locals.databaseConnection
+    res.locals.dbTables = Object.keys(models)
+    next()
   }
   
 }
