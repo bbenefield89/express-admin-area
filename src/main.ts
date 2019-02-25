fa587a4b0efad141f69104eef9dcfbc0156894d6
bbenefield89 <bsquared18@gmail.com>
2019-02-24 20:57:22 -0500

    PROPERLY IMPORT 'Admin' MODEL
    - src/main.ts
      - add all models to the 'databaseConnection' object

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import * as Sequelize from 'sequelize'
 
+import { Admin } from './models/Admin'
 import RoutesController from './controllers/RoutesController'
 import MiddlewareRegisterer from './middlewares/MiddlewareRegisterer/MiddlewareRegisterer'
 import ViewRegisterer from './middlewares/ViewRegisterer/ViewRegisterer'
@@ -10,13 +11,12 @@ class ExpressAdminArea {
   private static express;
   private static router: Router;
   private static databaseConnection;
-  private static databaseTables;
   
   public static init(express: any, databaseUri: string, databaseTables: object): Router {
     this.express = express
     this.router = express.Router()
     this.databaseConnection = new Sequelize(databaseUri)
-    this.databaseTables = databaseTables
+    this.databaseConnection.models = { Admin: Admin(this.databaseConnection), ...databaseTables }
     MiddlewareRegisterer.registerMiddlewares(this)
     RoutesController.registerAllRoutes(this.router)
     ViewRegisterer.registerViews(this)
