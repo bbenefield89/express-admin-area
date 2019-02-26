af5e112725079ec65e4fb7d822654dc152036761
bbenefield89 <bsquared18@gmail.com>
2019-02-25 20:36:47 -0500

    ADD CALL TO 'DBTablesService.getTables' WHICH RETURNS AN ARRAY OF TABLE NAMES
    - src/controllers/DBTablesController/DBTablesController.ts

--- src/controllers/DBTablesController/DBTablesController.ts
+++ src/controllers/DBTablesController/DBTablesController.ts
@@ -1,7 +1,10 @@
+import DashboardService from '../../services/DBTablesService/DBTablesService'
+
 class DashboardController {
 
   public static getTables(req, res): void {
-    res.send(res.locals.dbTables)
+    const dbTables: Array<string> = DashboardService.getTables(res.locals.databaseConnection)
+    res.send(dbTables)
   }
 
   public static getTableByName(req, res): void {
