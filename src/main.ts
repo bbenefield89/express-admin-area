98391c49ceef30da8de0e2f2fca2405c67225d7d
bbenefield89 <bsquared18@gmail.com>
2019-02-25 21:53:07 -0500

    NEW METHOD 'ExpressAdminArea.attachTablesToDbConnection' ATTACHES ARRAY OF DATABASE MODELS ONTO THE USERS DATABASE CONNECTION
    - src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -16,13 +16,30 @@ class ExpressAdminArea {
     this.express = express
     this.router = express.Router()
     this.databaseConnection = new Sequelize(databaseUri)
-    this.databaseConnection.models = { Admin: Admin(this.databaseConnection), ...databaseTables }
+    this.attachTablesToDbConnection(databaseTables)
     MiddlewareRegisterer.registerMiddlewares(this)
     RoutesController.registerAllRoutes(this.router)
     ViewRegisterer.registerViews(this)
     return this.router
   }
 
+  private static attachTablesToDbConnection(dbTables: any): void {
+    this.databaseConnection.models = [{ name: 'admin', model: Admin(this.databaseConnection) }]
+    const dbTableNames: Array<String> = Object.keys(dbTables)
+    let i: number = 0
+    for (let table in dbTables) {
+      this.databaseConnection.models.push({
+        name: this.dbTableNameLowerCased(dbTableNames[i]),
+        model: dbTables[table]
+      })
+      i++
+    }
+  }
+
+  private static dbTableNameLowerCased(dbTableName: String): String {
+    return dbTableName.toLowerCase()
+  }
+  
 }
 
 export { ExpressAdminArea }
