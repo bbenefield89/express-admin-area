6518635a3f1f5120c92a0c9cf7780fa6d612a3fb
bbenefield89 <bsquared18@gmail.com>
2019-02-28 21:41:52 -0500

    RETURNS DATABASE TABLE NAMES
    - src/services/DBTablesService/DBTablesService.ts

--- src/services/DBTablesService/DBTablesService.ts
+++ src/services/DBTablesService/DBTablesService.ts
@@ -1,11 +1,16 @@
-class DashboardService {
+class DBTablesService {
 
-  public static getTables(dbConnection: any): Array<string> {
-    const tables: Array<Object> = dbConnection.models
-    const tablesKeys: Array<string> = tables.map((table: any) => table.name)
-    return tablesKeys
+  // public static getTables(dbConnection: any): Array<string> {
+  //   const tables: Array<Object> = dbConnection.models
+  //   const tablesKeys: Array<string> = tables.map((table: any) => table.name)
+  //   return tablesKeys
+  // }
+
+  public static getTables(dbModels) {
+    const dbTables = Object.keys(dbModels)
+    return dbTables
   }
-
+  
   public static async getTableRows(dbModel): Promise<any> {
     let dbTableRows: Object | Array<Object> = null
     try {
@@ -18,4 +23,4 @@ class DashboardService {
   
 }
 
-export default DashboardService
+export default DBTablesService
