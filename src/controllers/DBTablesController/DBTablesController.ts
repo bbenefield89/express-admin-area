2093fefdf772f9ceac0f6844e3d0554490b0955a
bbenefield89 <bsquared18@gmail.com>
2019-02-28 21:41:06 -0500

    RETURNS ARRAY OF STRINGS CONTAINING DATABASE TABLE NAMES
    - src/controllers/DBTablesController/DBTablesController.ts

--- src/controllers/DBTablesController/DBTablesController.ts
+++ src/controllers/DBTablesController/DBTablesController.ts
@@ -1,23 +1,31 @@
-import DashboardService from '../../services/DBTablesService/DBTablesService'
+import DBTablesService from '../../services/DBTablesService/DBTablesService'
 
 class DashboardController {
 
   public static getTables(req, res): void {
-    const dbTables: Array<string> = DashboardService.getTables(res.locals.databaseConnection)
+    const dbTables: Array<String> = DBTablesService.getTables(res.locals.databaseConnection.models)
     res.send(dbTables)
   }
 
   public static async getTableByName(req, res): Promise<void> {
-    let dbTable: Object | Array<Object> = { message: 'No data found', status: 204 }
-    for (let table of res.locals.databaseConnection.models) {
-      const tableNameMatchesUrlParam = (table.name === req.params.table)
-      if (tableNameMatchesUrlParam) {
-        dbTable = await DashboardService.getTableRows(table.model)
-      }
-    }
-    res.send(dbTable)
+    const tableName: string = req.params.table
+    const model: Object = res.locals.databaseConnection.models[tableName]
+    const columns: Object | Array<Object> = await DashboardController.foo(model)
+    res.send(columns)
   }
 
+  // TODO: Move this method into 'DBTableService'
+  public static async foo(model: any): Promise<Object | Array<Object>> {
+    let columns: Object | Array<Object> = null
+    try {
+      columns = await model.findAll()
+    }
+    catch (error) {
+      columns = { message: 'No Content', status: 204 }
+    }
+    return columns
+  }
+  
 }
 
 export default DashboardController
