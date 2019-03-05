0eb68d9e8056dad62be1fdbdebb753f8ef137183
bbenefield89 <bsquared18@gmail.com>
2019-03-04 22:11:07 -0500

    NEW METHOD 'DBTablesController.createRow' CREATES NEW ROW IN SPECIFIED TABLE
    - src/controllers/DBTablesController/DBTablesController.ts
      - does not encrypt sensitive data (passwords, etc.)

--- src/controllers/DBTablesController/DBTablesController.ts
+++ src/controllers/DBTablesController/DBTablesController.ts
@@ -23,6 +23,21 @@ class DBTablesController {
 
   // This seems like it's going to be very difficult. I'm going to hold off on this until later
   public static createTable() {}
+
+  // Rewrite this into a service
+  public static createRow(req, res): any {
+    const table: any = req.params.table
+    const dbModel: any = res.locals.databaseConnection.models[table]
+    dbModel.create({ username: req.body.username, password: req.body.password })
+      .then(response => {
+        console.log('\n\n\n', response, '\n\n\n')
+        res.send({ user: req.body })
+      })
+      .catch(err => {
+        console.log('\n\n\n', err, '\n\n\n')
+        res.send({ error: 'error' })
+      })
+  }
   
 }
 
