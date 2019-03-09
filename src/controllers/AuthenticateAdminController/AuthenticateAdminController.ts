56b6e020a3b95316b00733334a19e23dacdea3eb
bbenefield89 <bsquared18@gmail.com>
2019-03-08 23:13:40 -0500

    WORKING VERSION TO AUTH ADMINS
    - src/controllers/AuthenticateAdminController/AuthenticateAdminController.ts

--- src/controllers/AuthenticateAdminController/AuthenticateAdminController.ts
+++ src/controllers/AuthenticateAdminController/AuthenticateAdminController.ts
@@ -1,7 +1,35 @@
+import * as bcrypt from 'bcrypt'
+
 class AuthenticateAdminController {
 
   constructor() {}
 
+  public static authAdmin(req, res): void {
+    const { username, password } = req.body
+    const Admin = res.locals.databaseConnection.models['admin']
+    Admin.findOne({ where: { username } })
+      .then(row => {
+        if (row !== null) {
+          bcrypt.compare(password, row.password, (err, match) => {
+            if (err) {
+              console.log('\n\n', err, '\n\n')
+              res.send(err)
+            }
+            else if (match === true) {
+              const { password, ...user } = row.dataValues
+              res.send(user)
+            }
+            else {
+              res.send('Wrong password')
+            }
+          })
+        }
+        else {
+          res.send({ message: 'No Content', status: 204 })
+        }
+      })
+  }
+  
   public static getAdminJwt(_req, res): void {
     res.send({ data: res.locals.token })
   }
