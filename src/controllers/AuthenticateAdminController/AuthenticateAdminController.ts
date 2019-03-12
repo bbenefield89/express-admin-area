53f7883147c3c64acadb9bdbb7b7c3a7d0d1fde5
bbenefield89 <bsquared18@gmail.com>
2019-03-11 23:00:37 -0400

    MODULARIZING 'AuthenticateAdminController.authAdmin'
    - controllers/AuthenticateAdminController/AuthenticateAdminController.ts

--- src/controllers/AuthenticateAdminController/AuthenticateAdminController.ts
+++ src/controllers/AuthenticateAdminController/AuthenticateAdminController.ts
@@ -3,47 +3,59 @@ import * as jwt from 'jsonwebtoken'
 
 class AuthenticateAdminController {
 
-  constructor() {}
+  constructor() { }
 
-  public static authAdmin(req, res): void {
+  public static async authAdmin(req, res): Promise<void> {
     const { username, password } = req.body
-    const Admin = res.locals.databaseConnection.models['admin']
-    Admin.findOne({ where: { username } })
-      .then(row => {
-        if (row !== null) {
-          bcrypt.compare(password, row.password, (err, match) => {
-            if (err) {
-              console.log('\n\n', err, '\n\n')
-              res.send(err)
-            }
-            else if (match === true) {
-              const { password, ...user } = row.dataValues
-              jwt.sign({ user }, 'expressadminarea', (err, token) => {
-                if (err) {
-                  console.log('\n\n', err, '\n\n')
-                  res.send(err)
-                }
-                else {
-                  console.log(token)
-                  res.status(200).send({ token, message: 'OK', status: 200 })
-                }
-              })
-            }
-            else {
-              res.status(401).send({ message: 'Unautherized', status: 401 })
-            }
-          })
-        }
-        else {
-          res.send({ message: 'No Content', status: 204 })
-        }
-      })
+    const AdminModel = res.locals.databaseConnection.models['admin']
+    const adminRow: any | null = await AdminModel.findOne({ where: { username } })
+    if (adminRow !== null) {
+      const isPasswordsMatch = await Promise.resolve(bcrypt.compare(password, adminRow.password))
+      if (isPasswordsMatch) {
+        const { password, ...admin } = adminRow.dataValues
+        const token: string = await Promise.resolve(jwt.sign({ admin }, 'expressadminarea'))
+        res.send({ token })
+      }
+      else {
+        res.send({})
+      }
+    }
+    else {
+      res.status(204).send({})
+    }
   }
-  
+
+  private static foo(plainPassword: string, adminRow: any) {
+    return Promise.resolve(bcrypt.compare(plainPassword, adminRow.password))
+
+    //   bcrypt.compare(plainPassword, adminRow.password, (err, match) => {
+    //     if (err) {
+    //       console.log('\n\n', err, '\n\n')
+    //       res.send(err)
+    //     }
+    //     else if (match === true) {
+    //       const { password, ...user } = adminRow.dataValues
+    //       jwt.sign({ user }, 'expressadminarea', (err, token) => {
+    //         if (err) {
+    //           console.log('\n\n', err, '\n\n')
+    //           res.status(500).send(err)
+    //         }
+    //         else {
+    //           res.status(200).send({ token })
+    //         }
+    //       })
+    //     }
+    //     else {
+    //       res.status(401).send({})
+    //     }
+    //   })
+    // }
+  }
+
   public static getAdminJwt(_req, res): void {
     res.send({ data: res.locals.token })
   }
-  
+
 }
 
 export default AuthenticateAdminController
