1a2eab0d9eb960779869d17432f94c2f05313c26
bbenefield89 <bsquared18@gmail.com>
2019-03-18 22:00:29 -0400

    CHANGE VARIABLE DECLARATION FROM 'let' -> 'const'
    - src/utilities/DataEncryptor/DataEncryptor.ts

--- src/utilities/DataEncryptor/DataEncryptor.ts
+++ src/utilities/DataEncryptor/DataEncryptor.ts
@@ -3,7 +3,7 @@ import * as bcrypt from 'bcrypt'
 class DataEncryptor {
 
   public static async encrypt(unencryptedData: string): Promise<string> {
-    let encryptedData: string = await bcrypt.hash(unencryptedData, 10)
+    const encryptedData: string = await bcrypt.hash(unencryptedData, 10)
     return encryptedData
   }
 
