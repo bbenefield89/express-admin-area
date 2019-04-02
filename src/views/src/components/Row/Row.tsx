d852260c9cea2009c0549ed65bf1f8d9368eddad
bbenefield89 <bsquared18@gmail.com>
2019-04-01 23:38:10 -0400

    REMOVE UNUSED METHODS & ADD LOGIC TO EXISTING METHODS
    - src/views/src/components/Row/Row.tsx

--- src/views/src/components/Row/Row.tsx
+++ src/views/src/components/Row/Row.tsx
@@ -5,6 +5,7 @@ import { Field } from '../Field/Field'
 type Props = {
   match?: any
   row?: any
+  fields?: any[]
 }
 
 type State = {
@@ -28,30 +29,14 @@ class Row extends Component<Props, State> {
     )
   }
 
-  componentDidMount() {
-    if (this.props.row) {
-      this.setStateFields(this.props.row)
-    }
-    else if (this.props.match) {
-      fetch(`/expressadminarea/api/${this.props.match.url}`)
-        .then(res => res.json())
-        .then(json => this.setStateFields(json))
-        .catch(err => console.log(err))
-    }
-  }
-
-  public setStateFields(row: any): void {
-    const fields: any[] = []
-    for (let key in row) {
-      fields.push(row[key])
-    }
-    this.setState({ fields })
-  }
-
   public renderFields(): any {
-    const fields: any[] = this.state.fields.map((field: any, idx: number): any => (
-      <Field key={idx} field={field} />
-    ))
+    const fields: any[] = []
+    if (this.props.fields) {
+      this.props.fields.forEach((field: any, idx: number): any => {
+        const fieldElement = <Field key={idx} field={field} />
+        fields.push(fieldElement)
+      })
+    }
     return fields
   }
 
