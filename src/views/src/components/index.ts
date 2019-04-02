718855bb173fb016802f71ef5728d88a782eb2b8
bbenefield89 <bsquared18@gmail.com>
2019-04-01 23:36:09 -0400

    IMPORT/EXPORT <RowContainer /> AND <RowEdit />
    - src/views/src/components/index.ts

--- src/views/src/components/index.ts
+++ src/views/src/components/index.ts
@@ -1,6 +1,9 @@
 import { Row } from './Row/Row'
 import { Table } from './Table/Table'
 
+export { RowContainer } from './RowContainer/RowContainer'
+export { RowEdit } from './RowEdit/RowEdit'
+
 export {
   Row,
   Table
