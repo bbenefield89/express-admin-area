2e5ef8be8a58f3cc994a8b83a9d5a45598765cf1
bbenefield89 <bsquared18@gmail.com>
2019-04-10 23:53:56 -0400

    REMOVE UNUSED EXPORTS
    - src/views/src/index.tsx

--- src/views/src/components/index.ts
+++ src/views/src/components/index.ts
@@ -1,6 +1,5 @@
 export { Field } from './Field/Field'
 export { FieldList } from './FieldList/FieldList'
+export { FieldListEdit } from './FieldListEdit/FieldListEdit'
 export { RowContainer } from './RowContainer/RowContainer'
-export { RowDisplay } from './RowDisplay/RowDisplay'
-export { RowEdit } from './RowEdit/RowEdit'
 export { Table } from './Table/Table'
