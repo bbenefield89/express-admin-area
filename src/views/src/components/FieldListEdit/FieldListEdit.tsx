a7f04de1fa5a8efd28f9e27d18cc6412411ee7b7
bbenefield89 <bsquared18@gmail.com>
2019-04-11 23:03:05 -0400

    PLACEHOLDER CLASS TO LATER RENDER LIST OF FIELDS THAT WILL BE EDITABLE FROM CLIENT SIDE
    - src/views/src/components/FieldListEdit/FieldListEdit.tsx

---
+++ src/views/src/components/FieldListEdit/FieldListEdit.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+type Props = {
+  fields: any[]
+}
+
+type State = {}
+
+class FieldListEdit extends Component<Props, State> {
+
+  render() {
+    return <h1>FieldListEdit</h1>
+  }
+  
+}
+
+export { FieldListEdit }
