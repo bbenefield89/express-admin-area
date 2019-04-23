0de37a2810d41fda40c269c99d91f1a713d84348
Brandon Benefield <bsquared18@gmail.com>
2019-04-22 21:36:22 -0400

    MAP OVER DATA FROM '/api/:table/:pk' AND PASS FIELD DATA TO <FieldListEdit />
    - src/components/RowEdit/RowEdit.tsx

--- src/views/src/components/RowEdit/RowEdit.tsx
+++ src/views/src/components/RowEdit/RowEdit.tsx
@@ -1,26 +1,35 @@
 import React, { Component } from 'react'
 
+import { FieldListEdit } from '../FieldListEdit/FieldListEdit'
+
 type Props = {
   location: { pathname: string }
 }
 
 type State = {
   fields: any[]
+  inputs: any[]
+}
+
+type Fields = {
+  [key: string]: string
 }
 
 class RowEdit extends Component<Props, State> {
   
   public state = {
-    fields: []
+    fields: [],
+    inputs: []
   }
   
   render() {
     return (
       <form>
         {this.state.fields.map((field: any, idx: number) => {
-          return <input key={idx} value={field} />
+          return (
+            <FieldListEdit field={field} key={idx} />
+          )
         })}
-
         <input type="submit" />
       </form>
     )
@@ -33,20 +42,30 @@ class RowEdit extends Component<Props, State> {
   public fetchRow() {
     fetch(`/expressadminarea/api${ this.props.location.pathname }`)
       .then(res => res.json())
-      .then(json => {
-        this.setStateFields(json)
+      .then(async (json) => {
+        const fields: any[] = this.createNewArrayFromData(json, this.stateFieldsCallBack)
+        const inputs: any[] = this.createNewArrayFromData(json, this.stateInputsCallBack)
+        this.setState({ fields, inputs })
       })
       .catch(err => console.log(err))
   }
 
-  public setStateFields(fields: any) {
-    const fieldValues = []
+  public createNewArrayFromData(fields: object, callBack: Function): any[] {
+    const returnArray: any[] = []
     for (let fieldName in fields) {
-      fieldValues.push(fields[fieldName])
+      returnArray.push(callBack(fields, fieldName))
     }
-    this.setState({ fields: [...fieldValues] })
+    return returnArray
   }
-  
+
+  public stateFieldsCallBack(fields: Fields, fieldName: string): string {
+    return fields[fieldName]
+  }
+
+  public stateInputsCallBack(fields: Fields, fieldName: string): object {
+    return { [fieldName]: fields[fieldName] }
+  }
+
 }
 
 export { RowEdit }
