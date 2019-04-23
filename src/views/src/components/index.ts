01b7d61418bf2e4de8aef27e7a380fafec04a372
Brandon Benefield <bsquared18@gmail.com>
2019-04-22 23:59:34 -0400

    IMPORT/EXPORT <FieldEdit />
    - src/components/index.ts

--- src/views/src/components/index.ts
+++ src/views/src/components/index.ts
@@ -1,5 +1,6 @@
 export { Field } from './Field/Field'
 export { FieldList } from './FieldList/FieldList'
+export { FieldEdit } from './FieldEdit/FieldEdit'
 export { FieldListEdit } from './FieldListEdit/FieldListEdit'
 export { RowContainer } from './RowContainer/RowContainer'
 export { RowEdit } from './RowEdit/RowEdit'
