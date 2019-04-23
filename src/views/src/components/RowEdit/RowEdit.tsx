43a7b9bb943f9f90c95a573751c8d4aa010c6c36
Brandon Benefield <bsquared18@gmail.com>
2019-04-23 00:01:21 -0400

    CHANGED ALGO TO RENDER FORM INPUTS AND RELATED DATA
    - src/components/RowEdit/RowEdit.tsx

--- src/views/src/components/RowEdit/RowEdit.tsx
+++ src/views/src/components/RowEdit/RowEdit.tsx
@@ -1,35 +1,25 @@
 import React, { Component } from 'react'
 
-import { FieldListEdit } from '../FieldListEdit/FieldListEdit'
+import { FieldEdit, FieldListEdit } from '../index'
 
 type Props = {
   location: { pathname: string }
 }
 
 type State = {
-  fields: any[]
-  inputs: any[]
-}
-
-type Fields = {
-  [key: string]: string
+  inputs: object
 }
 
 class RowEdit extends Component<Props, State> {
   
-  public state = {
-    fields: [],
+  state = {
     inputs: []
   }
   
   render() {
     return (
       <form>
-        {this.state.fields.map((field: any, idx: number) => {
-          return (
-            <FieldListEdit field={field} key={idx} />
-          )
-        })}
+        {this.renderFormInputs()}
         <input type="submit" />
       </form>
     )
@@ -38,32 +28,22 @@ class RowEdit extends Component<Props, State> {
   componentDidMount() {
     this.fetchRow()
   }
+
+  public renderFormInputs() {
+    const inputs = this.state.inputs.map((field: any[]) => {
+      return <FieldEdit key={field[0]} field={field} />
+    })
+    return inputs
+  }
   
   public fetchRow() {
     fetch(`/expressadminarea/api${ this.props.location.pathname }`)
       .then(res => res.json())
       .then(async (json) => {
-        const fields: any[] = this.createNewArrayFromData(json, this.stateFieldsCallBack)
-        const inputs: any[] = this.createNewArrayFromData(json, this.stateInputsCallBack)
-        this.setState({ fields, inputs })
+        const inputs: Array<any[]> = Object.entries(json)
+        this.setState({ inputs })
       })
       .catch(err => console.log(err))
-  }
-
-  public createNewArrayFromData(fields: object, callBack: Function): any[] {
-    const returnArray: any[] = []
-    for (let fieldName in fields) {
-      returnArray.push(callBack(fields, fieldName))
-    }
-    return returnArray
-  }
-
-  public stateFieldsCallBack(fields: Fields, fieldName: string): string {
-    return fields[fieldName]
-  }
-
-  public stateInputsCallBack(fields: Fields, fieldName: string): object {
-    return { [fieldName]: fields[fieldName] }
   }
 
 }
