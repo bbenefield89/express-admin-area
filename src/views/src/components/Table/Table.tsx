679319f226c5a7cdcdc9a3aba465cebdf30950da
Brandon Benefield <bsquared18@gmail.com>
2019-05-03 20:17:34 -0400

    PASS DOWN NECESSARRY PROPERTIES TO <CreateNewFormRow />
    - src/views/src/components/Table/Table.tsx

--- src/views/src/components/Table/Table.tsx
+++ src/views/src/components/Table/Table.tsx
@@ -29,7 +29,12 @@ class Table extends Component<Props, State> {
       <React.Fragment>
         <h1>Table</h1>
 
-        <CreateNewRowForm fields={this.state.fields} />
+        <CreateNewRowForm
+          fields={this.state.fields}
+          routerMatch={this.props.match}
+          createNewDbRow={this.createNewDbRow.bind(this)}
+          comp={this}
+        />
 
         {this.state.rows.map((row: any, idx: number): any => {
           return (
@@ -53,6 +58,24 @@ class Table extends Component<Props, State> {
       .then(rows => {
         const fields: string[] = Object.keys(rows[0])
         this.setState({ fields, rows })
+      })
+      .catch(err => console.log(err))
+  }
+
+  public createNewDbRow(inputFields: object|any): void {
+    const inputFieldValues: object|any = {}
+    for (let prop in inputFields) {
+      inputFieldValues[prop] = inputFields[prop].value
+    }
+    
+    fetch('/expressadminarea/api/tables/' + this.tableName, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(inputFieldValues)
+    })
+      .then(res => res.json())
+      .then((newRow: object|any) => {
+        this.setState({ rows: [...this.state.rows, newRow] })
       })
       .catch(err => console.log(err))
   }
