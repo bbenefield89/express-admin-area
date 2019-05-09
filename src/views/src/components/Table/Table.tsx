ed1dbb10f48eed0fa40c469b62ce00d0c4859f20
Brandon Benefield <bsquared18@gmail.com>
2019-05-08 20:15:36 -0400

    'this.shouldErrorElementRender()' DETERMINES IF ERROR MESSAGE SHOULD RENDER DEPENDING ON RESULT FROM CREATING A NEW ROW IN DB
    - src/views/src/components/Table/Table.tsx

--- src/views/src/components/Table/Table.tsx
+++ src/views/src/components/Table/Table.tsx
@@ -14,6 +14,7 @@ type Props = {
 type State = {
   fields: string[]
   rows: object[]
+  isErrorElementHidden: boolean
 }
 
 class Table extends Component<Props, State> {
@@ -21,7 +22,8 @@ class Table extends Component<Props, State> {
   public tableName: string = this.props.match.params.tableName
   public state = {
     fields: [],
-    rows: []
+    rows: [],
+    isErrorElementHidden: true
   }
 
   public render() {
@@ -33,6 +35,8 @@ class Table extends Component<Props, State> {
           fields={this.state.fields}
           createNewDbRow={this.createNewDbRow.bind(this)}
         />
+
+        {this.shouldErrorElementRender()}
 
         {this.state.rows.map((row: any, idx: number): any => {
           return (
@@ -60,22 +64,38 @@ class Table extends Component<Props, State> {
       .catch(err => console.log(err))
   }
 
-  public createNewDbRow(inputFields: object|any): void {
-    const inputFieldValues: object|any = {}
+  public createNewDbRow(inputFields: object | any): void {
+    const inputFieldValues: object | any = {}
     for (let prop in inputFields) {
       inputFieldValues[prop] = inputFields[prop].value
     }
-    
+
     fetch('/expressadminarea/api/tables/' + this.tableName, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(inputFieldValues)
     })
       .then(res => res.json())
-      .then((newRow: object|any) => {
-        this.setState({ rows: [...this.state.rows, newRow] })
+      .then((newRow: object | any) => {
+        if (newRow.status === 400) {
+          this.setState({ isErrorElementHidden: false })
+        }
+        else {
+          this.setState({
+            rows: [...this.state.rows, newRow],
+            isErrorElementHidden: true
+          })
+        }
       })
       .catch(err => console.log(err))
+  }
+
+  public shouldErrorElementRender(): JSX.Element | null {
+    let errorElement: JSX.Element | null = null
+    if (this.state.isErrorElementHidden === false) {
+      errorElement = <p>Error</p>
+    }
+    return errorElement
   }
 
 }
