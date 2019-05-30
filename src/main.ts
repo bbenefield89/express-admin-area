687d6a4111f1a3c859233a1b2635081d89d9b9f3
Brandon Benefield <bsquared18@gmail.com>
2019-05-29 22:41:29 -0400

    REMOVE UNNECESSARY METHODS/VARIABLES
    - src/main.ts
      - properties removed
        - config
    
      - params removed
        - config
    
      - methods removed
        - attachTablesToDbConnection()

--- src/main.ts
+++ src/main.ts
@@ -5,39 +5,26 @@ import { Admin } from './models/Admin'
 import RoutesController from './controllers/RoutesController'
 import MiddlewareRegisterer from './middlewares/MiddlewareRegisterer/MiddlewareRegisterer'
 import ViewRegisterer from './middlewares/ViewRegisterer/ViewRegisterer'
+import { ModelConfigurator } from './utilities/index'
 
 class ExpressAdminArea {
 
   private static express;
   private static router: Router;
   private static databaseConnection;
-  private static config: object;
   
-  public static init(express: any, databaseUri: string, databaseTables: object, config: object): Router {
+  public static init(express: any, databaseUri: string, databaseTables: object[]): Router {
     this.express = express
     this.router = express.Router()
     this.databaseConnection = new Sequelize(databaseUri)
-    this.config = config
-    this.attachTablesToDbConnection(databaseTables)
+    this.databaseConnection.models = {
+      admin: Admin(this.databaseConnection),
+      ...ModelConfigurator.configureModels(databaseTables)
+    }
     MiddlewareRegisterer.registerMiddlewares(this)
     RoutesController.registerAllRoutes(this.router)
     ViewRegisterer.registerViews(this)
     return this.router
-  }
-
-  /**
-   * TODO: Figure out what to do with these methods below
-   *       They don't feel like they should be directly associated with the 'ExpressAdminArea' class
-   * 
-   * TODO: Should this method return an object of database tables to make accessing specific tables via the
-   *       'req.params' object O(1) instead of an array of objects which is O(n)
-   */
-  private static attachTablesToDbConnection(dbTables: any): void {
-    this.databaseConnection.models = { admin: Admin(this.databaseConnection) }
-    for (let table in dbTables) {
-      const lowerCasedTableName: string = table.toLowerCase()
-      this.databaseConnection.models[lowerCasedTableName] = dbTables[table]
-    }
   }
 
 }
