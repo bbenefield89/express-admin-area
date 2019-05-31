db8fed1c7eafdb5555377f29b869c3c94639475e
Brandon Benefield <bsquared18@gmail.com>
2019-05-30 23:25:56 -0400

    NEW METHOD 'DBTablesController.getTableFieldNames()' RETURNS ALL TABLE FIELD NAMES FROM A SPECIFIC TABLE
    - src/controllers/DBTablesController/DBTablesController.ts
    - src/controllers/RoutesController.ts

--- src/controllers/RoutesController.ts
+++ src/controllers/RoutesController.ts
@@ -26,6 +26,7 @@ class RoutesController {
   private static registerDBTablesRoutes(): void {
     this.router.get(this.expressAdminArea + '/api/tables', DBTablesController.getTables)
     this.router.get(this.expressAdminArea + '/api/tables/:table', DBTablesController.getTableRows)
+    this.router.get(this.expressAdminArea + '/api/tables/:table/describe', DBTablesController.getTableFieldNames)
     this.router.get(this.expressAdminArea + '/api/tables/:table/:pk', DBTablesController.getTableRowByPk)
     this.router.post(this.expressAdminArea + '/api/tables/:table', DBTablesController.createRow)
     this.router.patch(this.expressAdminArea + '/api/tables/:table/:pk', DBTablesController.updateRow)
